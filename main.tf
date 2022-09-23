29a30c3bc5f4a00efd7e26b174d05c2261ddd7f4
Nishi-080893 <114211137+Nishi-080893@users.noreply.github.com>
2022-09-23 12:10:34 +0530

    changed instance type

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "nishiawsserver" {
   ami = var.image
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
  # key_name = "nishi-new"
   tags = {
     Name = lower (var.name)
